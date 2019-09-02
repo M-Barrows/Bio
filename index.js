995a52f8acc5e2dd1876eee439e5883ab6a8b81b
MBarrows20 <michael.barrows.a@gmail.com>
2019-09-02 02:33:13 -0400

    updated main information

--- index.js
+++ index.js
@@ -6,26 +6,60 @@ var app = new Vue({
             "lastName":"Barrows",
             "age": 26,
             "location":"Cincinnati, OH",
-            "title":"Business Intelligence Analyst",
+            "title":"Business Intelligence Analyst"
         },
-        "socials":{
-            "github":"https://github.com/MBarrows20",
-            "linkedin":"https://www.linkedin.com/in/michaelabarrows",
-            "twitter":""        
-        },
+        "socials":[
+            {
+                "name":"GitHub",
+                "link":"https://github.com/MBarrows20",
+                "icon":"fab fa-github"
+            },
+            {
+                "name":"LinkedIn",
+                "link":"https://www.linkedin.com/in/michaelabarrows",
+                "icon":"fab fa-linkedin"
+            }      
+        ],
         "cards":{
             "Education":[
                 {
                     "degree":"M.S. Info Systems Management",
                     "school":"University of Cincinnati",
                     "year":"2018.12",
-                    "description":"UC provided me with a focused understanding of info systems management solutions as well as a deeper knowledge of statistical analysis using R, Tableau, and SPSS."
+                    "description":"UC provided me with a focused understanding of info systems management solutions as well as a deeper knowledge of statistical analysis using R, Tableau, and SPSS.",
+                    "courses":[
+                        "Data Mining for Business Intelligence"
+                        ,"IS Project Management"
+                        ,"Advanced Web Developement w/ .NET"
+                        , "IS Security"
+                        , "Database Design"
+                        ,"Data Warehousing and Business Intelligence"
+                    ]
+                },
+                {
+                    "degree":"Data Analytics Graduate Certificate",
+                    "school":"University of Cincinnati",
+                    "year":"2018.12",
+                    "description":"",
+                    "courses":[
+                        "Statistical Computing"
+                        ,"Data Visualization"
+                        ,"Data Wrangling w/ R"
+                        ,"Data Analysis Methods"
+                    ]
                 },
                 {
                     "degree":"B.S. Biology",
                     "school":"Hope College",
                     "year":"2015.05",
-                    "description":"At Hope College I learned the fundamentals of academic research, ecological sciences, and higher-level problem solving."
+                    "description":"At Hope College I learned the fundamentals of academic research, ecological sciences, and higher-level problem solving.",
+                    "courses":[
+                        "Software Design and Implementation"
+                        , "Conservation Biology"
+                        , "Evolutionary Biology"
+                        , "Vascular Plant Systematics"
+                        , "Ancient Philosophy"
+                    ]
                 }
             ],
             "Skills":[
@@ -57,7 +91,8 @@ var app = new Vue({
                     "start":"2018.05",
                     "end":"",
                     "current":"yes",
-                    "description":"Using advanced analytical techniques to drive business decisions."
+                    "description":"At TQL I'm using advanced analytical techniques in conjunction with tools such as Power BI, SSRS, and R Shiny to deliver actionable insights that help drive influential change.",
+                    "moreinfo":""
                 },
                 {
                     "company":"Lane Public Library",
@@ -65,7 +100,8 @@ var app = new Vue({
                     "start":"2017.06",
                     "end":"2018.05",
                     "current":"no",
-                    "description":""
+                    "description":"At the LPL Technology Center I was able to assist patrons - both individually and in groups - to navigate technology they found troubling. I also took pride in showcasing new hardware, software, or methodologies that offered numerous benefits to our patrons and nearby businesses.",
+                    "moreinfo":""
                 },
                 {
                     "company":"University of Michigan",
@@ -73,7 +109,18 @@ var app = new Vue({
                     "start":"2015.05",
                     "end":"2016.11",
                     "current":"no",
-                    "description":""
+                    "description":"Working under the supervision of Dr. Deborah Goldberg, I investigated the effects of land-use and climate changes on wetland environments. In this position I was responsible for sample collection, data entry, data management, data analysis, site maintenance, resource purchasing, and employee management. Read more about the project here: ",
+                    "moreinfo":"http://biostation.lsa.umich.edu/projects/experimental-invasion-of-constructed-wetland-mesocosms.htm"
+                }
+            ],
+            "Volunteer":[
+                {
+                    "company":"University of Cincinnati",
+                    "start":"2017.06",
+                    "end":"2018.12",
+                    "current":"no",
+                    "description":"I worked in Dr. Ishi Buffam's laboratory on a project relating to urban green roof architecture. As a volunteer research assistant I was responsible for the cleaning, consolidation, and analysis of data collected from environmental data loggers. This project allowed me to combine my interests in Data Management and Analysis with my background and love for ecology. It also allows me to provide valuable insight into the efficacy of green roof architecture through flow analysis of water across the roof before, during, and after rain events.",
+                    "moreinfo":""
                 }
             ],
             "Projects":[
@@ -84,7 +131,8 @@ var app = new Vue({
                         "R Shiny",
                         "Similarity Matricies",
                         "Network Maps"
-                    ]
+                    ],
+                    "link":"#"
                 },
                 {
                     "name":"Safe Pedals",
@@ -92,9 +140,10 @@ var app = new Vue({
                         "Dashboard",
                         "R Programming",
                         "Data Wrangling"
-                    ]
+                    ],
+                    "link":"#"
                 }
             ]
-        }
+        }        
     }
 });
